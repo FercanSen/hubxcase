6bb74a7cb24d220e84ade708c2ffe47f061ca7ea
FercanSen <fercansen@gmail.com>
2024-07-25 09:52:17 +0300

    Add sign and stamp page

---
+++ src/pages/SignAndStampPage/SignAndStampPage.tsx
@@ -0,0 +1,39 @@
+import '../PageContent.scss';
+import './SignAndStampPage.scss';
+
+function SignAndStampPage() {
+  return (
+    <div className="page-content">
+      <div className="page-content__text">
+        <h1>SIGN & STAMP</h1>
+        <h2>One-Tap Focus</h2>
+        <p>
+          Draw, scan or import your signature and stamp with a simple touch.
+          Sign and stamp any document with just a single tap!
+        </p>
+        <button>Learn More</button>
+      </div>
+      <div className="page-content__image">
+        <img
+          className="page-content__image--signAndStamp"
+          src="src/images/SignAndStamp/SignAndStamp.png"
+          alt="Sign & Stamp"
+        />
+        <div className="page-content__image--signAndStamp--overlay">
+          <img
+            className="page-content__image--signAndStamp--overlay sign"
+            src="src/images/SignAndStamp/Sign.png"
+            alt="Sign"
+          />
+          <img
+            className="page-content__image--signAndStamp--overlay stamp"
+            src="src/images/SignAndStamp/Stamp.png"
+            alt="Stamp"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignAndStampPage;
