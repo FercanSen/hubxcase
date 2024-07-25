625cd76e78201378a867cea7d047b2e97738719a
FercanSen <fercansen@gmail.com>
2024-07-25 13:07:10 +0300

    Add export and share page

--- src/pages/ExportAndSharePage/ExportAndSharePage.tsx
+++ src/pages/ExportAndSharePage/ExportAndSharePage.tsx
@@ -1,4 +1,5 @@
 import '../PageContent.scss';
+import './ExportAndSharePage.scss';
 
 function ExportAndSharePage() {
   return (
@@ -10,7 +11,35 @@ function ExportAndSharePage() {
           <p>Export your scans as PDF,JPG,ZIP,TXT and Word.</p>
           <button>Learn More</button>
         </div>
-        <div className="page-content__image">image-content</div>
+        <div className="page-content__image">
+          <img
+            className="export-and-share"
+            src="src/images/ExportAndShare/ExportAndShare.png"
+            alt="Export and Share"
+          />
+          <div className="file-formats">
+            <img
+              className="arrow rotateInUpLeft"
+              src="src/images/ExportAndShare/arrow.png"
+              alt="Export and Share Arrow"
+            />
+            <img
+              className="pdf rotateInUpLeft"
+              src="src/images/ExportAndShare/pdf.png"
+              alt="Export and Share PDF"
+            />
+            <img
+              className="jpg rotateInUpLeft"
+              src="src/images/ExportAndShare/jpg.png"
+              alt="Export and Share JPG"
+            />
+            <img
+              className="txt rotateInUpLeft"
+              src="src/images/ExportAndShare/txt.png"
+              alt="Export and Share TXT"
+            />
+          </div>
+        </div>
       </div>
     </>
   );
